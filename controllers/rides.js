60a3fe8f1458b1904e41372d04256280b92270f0
Emmy <emilyileeva@gmail.com>
2024-05-07 10:14:46 -0700

    update crud

--- controllers/rides.js
+++ controllers/rides.js
@@ -1,15 +1,5 @@
 const Ride = require("../models/Ride");
 
-exports.createRide = async (req, res) => {
-  try {
-    const ride = await Ride.create(req.body);
-    res.status(201).json(ride);
-  } catch (error) {
-    console.error("Error creating ride:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
 exports.getRides = async (req, res) => {
   try {
     const rides = await Ride.find();
