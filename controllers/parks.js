8d19f4e395e86076edeb531359e268f4328dc6fa
yongZhang <kinmer7@gmail.com>
2024-05-07 10:24:48 -0400

    add parks controller

--- controllers/parks.js
+++ controllers/parks.js
@@ -1,10 +1,54 @@
 const express = require("express");
 const { Park } = require("../models");
 
-const Index = async (req, res, next) => {
-  try {
-    res.json(await Park.find({}));
-  } catch (error) {
-    res.status(400).json(error);
-  }
+const index = async (req, res, next) => {
+    try {
+        res.json(await Park.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 };
+
+const create = async (req, res, next) => {
+    try{
+        res.json(await Park.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+const show = async (req, res, next) => {
+    try {
+        res.json(await Park.findById(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+const  destroy = async (req,res,next) => {
+    try {
+        res.json(await Park.findByIdAndDelete(req.params.id));
+      } catch (error) {
+        res.status(400).json(error);
+      }
+}
+
+const update = async (req, res, next) => {
+    try {
+        res.json(
+          await Park.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        );
+      } catch (error) {
+        res.status(400).json(error);
+      }
+}
+
+
+
+module.exports = {
+    index,
+    create,
+    show,
+    delete: destroy,
+    update
+}
