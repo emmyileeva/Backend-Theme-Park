438f152908625bf6710d0c1dd81aaf4bf7e0958e
yana1al <alimasharma@gmail.com>
2024-05-07 14:17:57 -0700

    Debugging routes

--- routes/parks.js
+++ routes/parks.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const parksController = require("../controllers/parks");
-const { Park } = require("../models/Park");
-
+const Park = require("../models/park"); 
 router.get("/", parksController.getParks);
 
 module.exports = router;
