011c541699cca53f21a4ee91b3e20b445c26d9dd
Emmy <emilyileeva@gmail.com>
2024-05-07 07:22:52 -0700

    set up server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
+require("./config/db.connection");
 const { PORT } = process.env;
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+const parksRouter = require("./routes/parks");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use("/parks", parksRouter);
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
