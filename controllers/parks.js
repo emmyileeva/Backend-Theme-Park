a685feb00bf3f792720ece8de7d3791e81633391
pdarcy <pdarcy@riseup.net>
2024-05-08 09:10:07 -0400

    get backend running

--- controllers/parks.js
+++ controllers/parks.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const { Park } = require("../models/park");
+const { Park } = require("../models/Park");
 
 
 exports.getParks = async (req, res) => {
--- routes/parks.js
+++ routes/parks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const parksController = require("../controllers/parks");
-const Park = require("../models/park"); 
+const Park = require("../models/Park"); 
 router.get("/", parksController.getParks);
 
 module.exports = router;
